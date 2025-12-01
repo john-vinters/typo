03ef3a07831e4326a3b03f534db5d6960eb17697
John Vinters <john.vinters@gmail.com>
2025-12-01 15:16:05 +0000

    Add Typo.Font.StandardFont.

---
+++ lib/typo/font/standard_font.ex
@@ -0,0 +1,63 @@
+#
+# (c) Copyright 2025, John Vinters <john.vinters@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Typo.Font.StandardFont do
+  @moduledoc false
+
+  @type t :: %__MODULE__{
+          ascender: number(),
+          attributes: list(),
+          cap_height: number(),
+          cmap: %{optional(String.codepoint()) => Typo.glyph()},
+          descender: number(),
+          encoding: String.t(),
+          family_name: String.t(),
+          flags: non_neg_integer(),
+          font_name: String.t(),
+          is_fixed_pitch: boolean(),
+          is_italic: boolean(),
+          italic_angle: number(),
+          kern: %{optional({Typo.glyph(), Typo.glyph()}) => number()},
+          stem_h: number(),
+          stem_v: number(),
+          underline_position: number(),
+          underline_thickness: number(),
+          x_height: number(),
+          weight: String.t(),
+          width: %{optional(Typo.glyph()) => number()}
+        }
+
+  defstruct ascender: 0,
+            attributes: [],
+            cap_height: 0,
+            cmap: %{},
+            descender: 0,
+            encoding: "AdobeStandardEncoding",
+            family_name: "",
+            flags: 0,
+            font_name: "",
+            is_fixed_pitch: false,
+            is_italic: false,
+            italic_angle: 0.0,
+            kern: %{},
+            stem_h: 0,
+            stem_v: 0,
+            underline_position: 0,
+            underline_thickness: 0,
+            x_height: 0,
+            weight: "MEDIUM",
+            width: %{}
+end
