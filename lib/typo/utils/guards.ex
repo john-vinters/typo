69ece2c0981aee2c77b0604df4e35bfb062ed91c
John Vinters <john.vinters@gmail.com>
2026-04-11 19:21:30 +0100

    Rework Typo.Utils.Guards, and add tests.

--- lib/typo/utils/guards.ex
+++ lib/typo/utils/guards.ex
@@ -17,13 +17,28 @@
 defmodule Typo.Utils.Guards do
   @moduledoc false
 
+  defguard is_colour_range(c) when c >= 0.0 and c <= 1.0
+  defguard is_colour(c, n) when is_colour_range(elem(c, n))
+  defguard is_colour_greyscale(c) when is_colour_range(c)
+  defguard is_tuple(t, n) when is_tuple(t) and tuple_size(t) == n
+  defguard is_number(t, n) when is_number(elem(t, n))
+
+  defguard is_colour_cmyk(c)
+           when is_tuple(c, 4) and is_colour(c, 0) and is_colour(c, 1) and is_colour(c, 2) and
+                  is_colour(c, 3)
+
+  defguard is_colour_rgb(c)
+           when is_tuple(c, 3) and is_colour(c, 0) and is_colour(c, 1) and is_colour(c, 2)
+
   defguard is_page_number(n) when is_integer(n)
 
   defguard is_page_orientation(o) when o in [:landscape, :portrait]
 
   defguard is_page_rotation(r) when r in [0, 90, 180, 270]
 
-  defguard is_page_size(s)
-           when is_tuple(s) and tuple_size(s) == 2 and is_number(elem(s, 0)) and
-                  is_number(elem(s, 1))
+  defguard is_page_size(s) when is_tuple(s, 2) and is_number(s, 0) and is_number(s, 1)
+
+  defguard is_transform_matrix(m)
+           when is_tuple(m, 6) and is_number(m, 0) and is_number(m, 1) and is_number(m, 2) and
+                  is_number(m, 3) and is_number(m, 4) and is_number(m, 5)
 end
---
+++ test/guard_test.exs
@@ -0,0 +1,101 @@
+defmodule GuardTest do
+  use ExUnit.Case
+  import Typo.Utils.Guards
+
+  test "is_colour_cmyk" do
+    assert is_colour_cmyk({0.0, 0.0, 0.0, 0.0}) == true
+    assert is_colour_cmyk({0.5, 0.5, 0.5, 0.5}) == true
+    assert is_colour_cmyk({1.0, 1.0, 1.0, 1.0}) == true
+    assert is_colour_cmyk({2.0, 0.5, 0.5, 0.5}) == false
+    assert is_colour_cmyk({0.5, 2.0, 0.5, 0.5}) == false
+    assert is_colour_cmyk({0.5, 0.5, 2.0, 0.5}) == false
+    assert is_colour_cmyk({0.5, 0.5, 0.5, 2.0}) == false
+    assert is_colour_cmyk({-0.5, 0.5, 0.5, 0.5}) == false
+    assert is_colour_cmyk({0.5, -0.5, 0.5, 0.5}) == false
+    assert is_colour_cmyk({0.5, 0.5, -0.5, 0.5}) == false
+    assert is_colour_cmyk({0.5, 0.5, 0.5, -0.5}) == false
+    assert is_colour_cmyk(0.0) == false
+    assert is_colour_cmyk(:test) == false
+    assert is_colour_cmyk({0, 0, 0}) == false
+    assert is_colour_cmyk({:a, :b, :c, :d}) == false
+  end
+
+  test "is_colour_greyscale" do
+    assert is_colour_greyscale(0.0) == true
+    assert is_colour_greyscale(0.5) == true
+    assert is_colour_greyscale(1.0) == true
+    assert is_colour_greyscale(-0.5) == false
+    assert is_colour_greyscale(1.5) == false
+    assert is_colour_greyscale(:test) == false
+    assert is_colour_greyscale({0, 0, 0}) == false
+  end
+
+  test "is_colour_rgb" do
+    assert is_colour_rgb({0.0, 0.0, 0.0}) == true
+    assert is_colour_rgb({0.5, 0.5, 0.5}) == true
+    assert is_colour_rgb({1.0, 1.0, 1.0}) == true
+    assert is_colour_rgb({2.0, 0.5, 0.5}) == false
+    assert is_colour_rgb({0.5, 2.0, 0.5}) == false
+    assert is_colour_rgb({0.5, 0.5, 2.0}) == false
+    assert is_colour_rgb({-0.5, 0.5, 0.5}) == false
+    assert is_colour_rgb({0.5, -0.5, 0.5}) == false
+    assert is_colour_rgb({0.5, 0.5, -0.5}) == false
+    assert is_colour_rgb(0.0) == false
+    assert is_colour_rgb(:test) == false
+    assert is_colour_rgb({:a, :b, :c}) == false
+    assert is_colour_rgb({0, 0, 0, 0}) == false
+  end
+
+  test "is_page_number" do
+    assert is_page_number(0) == true
+    assert is_page_number(1) == true
+    assert is_page_number(10) == true
+    assert is_page_number(:test) == false
+    assert is_page_number(0.0) == false
+    assert is_page_number({1.0, 2.0}) == false
+  end
+
+  test "is_page_orientation" do
+    assert is_page_orientation(:portrait) == true
+    assert is_page_orientation(:landscape) == true
+    assert is_page_orientation(:something) == false
+    assert is_page_orientation(0) == false
+    assert is_page_orientation([]) == false
+    assert is_page_orientation({1.0, 2.0}) == false
+  end
+
+  test "is_page_rotation" do
+    assert is_page_rotation(0) == true
+    assert is_page_rotation(90) == true
+    assert is_page_rotation(180) == true
+    assert is_page_rotation(270) == true
+    assert is_page_rotation(45) == false
+    assert is_page_rotation(90.0) == false
+    assert is_page_rotation(:test) == false
+    assert is_page_rotation([]) == false
+    assert is_page_rotation({1.0, 2.0}) == false
+  end
+
+  test "is_page_size" do
+    assert is_page_size({0, 0}) == true
+    assert is_page_size({50, 45}) == true
+    assert is_page_size({50.0, 45}) == true
+    assert is_page_size({45, 50.0}) == true
+    assert is_page_size({0, 1, 2}) == false
+    assert is_page_size({50, :test}) == false
+    assert is_page_size(:test) == false
+    assert is_page_size([]) == false
+    assert is_page_size({:a, :b}) == false
+  end
+
+  test "is_transform_matrix" do
+    assert is_transform_matrix({0, 0, 0, 0, 0, 0}) == true
+    assert is_transform_matrix({0.0, 1.0, 2.0, 3.0, 4.0, 5.0}) == true
+    assert is_transform_matrix({1, 2, 3, 4, 5, 6}) == true
+    assert is_transform_matrix({1, 2, 3, 4, 5}) == false
+    assert is_transform_matrix({1, 2, 3, 4, 5, 6, 7}) == false
+    assert is_transform_matrix(:test) == false
+    assert is_transform_matrix([]) == false
+    assert is_transform_matrix({:a, :b, :c, :d, :e, :f}) == false
+  end
+end
