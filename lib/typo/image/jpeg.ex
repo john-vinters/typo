4d264fce1abc211f57f1e12d463eb652717a204b
John Vinters <john.vinters@gmail.com>
2026-04-16 19:53:30 +0100

    Add Typo.Image.JPEG.

---
+++ lib/typo/image/jpeg.ex
@@ -0,0 +1,116 @@
+#
+# (c) Copyright 2026, John Vinters <john.vinters@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+defmodule Typo.Image.JPEG do
+  @moduledoc """
+  JPEG image support.
+  """
+
+  alias Typo.Image.JPEG
+
+  @opaque t :: %__MODULE__{
+            width: non_neg_integer(),
+            height: non_neg_integer(),
+            bits_per_component: non_neg_integer(),
+            components: non_neg_integer(),
+            colour_space: :DeviceCMYK | :DeviceGray | :DeviceRGB,
+            data: binary()
+          }
+
+  defstruct width: 0,
+            height: 0,
+            bits_per_component: 0,
+            components: 0,
+            colour_space: :DeviceRGB,
+            data: <<>>
+
+  defguard is_supported_sof(t) when t in 0xC0..0xCF and t not in [0xC4, 0xCC]
+
+  @doc """
+  Returns `true` if the passed binary starts with a valid JPEG magic number.
+  """
+  @spec jpeg?(binary()) :: boolean()
+  def jpeg?(<<255::8, 216::8, _rest::binary>>), do: true
+  def jpeg?(_), do: false
+
+  @doc """
+  Processes a JPEG binary, returning filled-in struct if successful, or raising
+  `Typo.ImageError` if there was a problem.
+  """
+  @spec process!(binary()) :: JPEG.t()
+  def process!(<<255::8, 216::8, rest::binary>> = jpeg), do: process_segments(jpeg, rest)
+  def process!(_), do: raise(Typo.ImageError, "binary doesn't appear to be a JPEG image")
+
+  # processes JPEG file segments, extracting information.
+  @spec process_segments(binary(), binary()) :: JPEG.t()
+  defp process_segments(jpeg, <<255::8, type::8, rest::binary>>) do
+    case type do
+      t when is_supported_sof(t) -> process_sof(jpeg, rest)
+      t when t in [0xD9, 0xDA] -> raise Typo.ImageError, "unsupported JPEG image type"
+      _other -> process_segments(jpeg, skip_segment(rest))
+    end
+  end
+
+  defp process_segments(_, _), do: raise(Typo.ImageError, "JPEG image appears to be corrupt")
+
+  # extracts information from supported SOF segments.
+  @spec process_sof(binary(), binary()) :: JPEG.t()
+  def process_sof(jpeg, <<_l::16, p::8, h::16, w::16, c::8, _r::binary>>),
+    do: %JPEG{
+      width: w,
+      height: h,
+      bits_per_component: p,
+      components: c,
+      colour_space: to_colour_space(c),
+      data: jpeg
+    }
+
+  def process_sof(_, _), do: raise(Typo.ImageError, "JPEG image appears to be corrupt")
+
+  # converts component count to a colour space.
+  @spec to_colour_space(byte()) :: :DeviceCMYk | :DeviceGray | :DeviceRGB
+  defp to_colour_space(1), do: :DeviceGray
+  defp to_colour_space(3), do: :DeviceRGB
+  defp to_colour_space(4), do: :DeviceCMYK
+
+  defp to_colour_space(c),
+    do: raise(Typo.ImageError, "unsupported JPEG image colour space: #{inspect(c)}")
+
+  # skips a JPEG image segment.
+  @spec skip_segment(binary()) :: binary()
+  defp skip_segment(<<length::16, _skip::binary-size(length - 2), rest::binary>>), do: rest
+  defp skip_segment(_), do: <<>>
+
+  defimpl Typo.Protocol.Object, for: JPEG do
+    @spec to_iodata(JPEG.t(), Keyword.t()) :: iodata()
+    def to_iodata(this, options) do
+      alias Typo.Protocol.Object
+
+      dict = %{
+        :Type => :XObject,
+        :Subtype => :Image,
+        :Height => this.height,
+        :Width => this.width,
+        :Filter => :DCTDecode,
+        :ColorSpace => this.colour_space,
+        :BitsPerComponent => this.bits_per_component,
+        :Length => byte_size(this.data)
+      }
+
+      [Object.to_iodata(dict, options), "\nstream\n", this.data, "\nendstream"]
+    end
+  end
+end
